0aa7b256a661337f02076cbb57ff37966740f604
github username <github email address>
2019-09-21 12:04:58 +0100

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   
-  resources :students, only: [:index, :new, :create]
-  # get "/students", to: "students#index", as: "students" 
+  # resources :students, only: [:index, :new, :create]
+  get "/students", to: "students#index", as: "students" 
+  get "/students/new", to: "students#new", as: "new_student"
   get "/students/:id", to: "students#show", as: "student" 
-  # get "/students/new", to: "students#new", as: "new_student" 
-  # post "/students/", to: "students#create"
+  post "/students/", to: "students#create"
   # get "/students/:id/edit", to: "students#edit", as: "edit_student" 
   # patch "/students/:id", to: "students#update"                     
   # delete "/students/:id", to: "students#destroy"
